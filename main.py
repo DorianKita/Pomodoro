4ed2e068cfda47a066bac49d8cf4cb5ac06c83e4
Dorian Kita <dorian.kita90@gmail.com>
2025-01-28 18:05:26 +0100

    created tomato image in tkinter canvas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import tkinter
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+
+canvas = tkinter.Canvas(width= 200, height=224)
+tomato = tkinter.PhotoImage(file= "tomato.png")
+canvas.create_image(100,112, image= tomato)
+canvas.pack()
+
+window.mainloop()
