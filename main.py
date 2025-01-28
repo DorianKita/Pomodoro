a5996e7ec35eb2c08fb879bd0ac7ee5058fc51e4
Dorian Kita <dorian.kita90@gmail.com>
2025-01-28 18:54:58 +0100

    created rest of pomodoro layout

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECKMARK = "✔"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -24,7 +25,19 @@ canvas = tkinter.Canvas(width=200,height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100,112, image= tomato_img)
 canvas.create_text(100,130, text="00:00", font=(FONT_NAME, 30, "bold"), fill="white")
-canvas.pack()
+canvas.grid(row= 1, column= 1)
+
+label = tkinter.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 45, "normal"), bg=YELLOW)
+label.grid(row= 0, column= 1)
+
+start_button = tkinter.Button(text="Start")
+start_button.grid(row= 2, column=0)
+
+reset_button = tkinter.Button(text="Reset")
+reset_button.grid(row= 2, column=2)
+
+checkmark = tkinter.Label(text=CHECKMARK, foreground=GREEN, bg=YELLOW, font=(FONT_NAME, 30, "normal"))
+checkmark.grid(row= 3, column=1)
 
 
 
