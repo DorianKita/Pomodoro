7fd61e1112860153dc752bbfca89074ada0104e6
Dorian Kita <dorian.kita90@gmail.com>
2025-01-28 18:37:17 +0100

    created canvas with tomato image and background color

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = tkinter.Canvas(width= 200, height=224)
-tomato = tkinter.PhotoImage(file= "tomato.png")
-canvas.create_image(100,112, image= tomato)
+canvas = tkinter.Canvas(width=200,height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(100,112, image= tomato_img)
+canvas.create_text(100,130, text="00:00", font=(FONT_NAME, 30, "bold"), fill="white")
 canvas.pack()
+
+
 
 window.mainloop()
