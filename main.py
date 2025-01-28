d3e3b9e6532626745e13568b8224cf0bccb5167b
Dorian Kita <dorian.kita90@gmail.com>
2025-01-28 20:58:16 +0100

    added changing title and font color, start timer function to set working and break sessions.

--- main.py
+++ main.py
@@ -11,14 +11,27 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECKMARK = "✔"
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_secs = WORK_MIN * 60
+    short_break_secs = SHORT_BREAK_MIN * 60
+    long_break_secs = LONG_BREAK_MIN * 60
 
+    if reps % 8 ==0:
+        count_down(long_break_secs)
+        label.config(text="Break", fg=GREEN)
+    elif reps % 2 == 0:
+        count_down(short_break_secs)
+        label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_secs)
+        label.config(text="Work", fg=RED)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
 
@@ -31,6 +44,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text= f"{floor(count_min)}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count -1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
@@ -43,8 +58,6 @@ tomato_img = tkinter.PhotoImage(file="tomato.png")
 canvas.create_image(100,112, image= tomato_img)
 timer_text = canvas.create_text(100,130, text="00:00", font=(FONT_NAME, 30, "bold"), fill="white")
 canvas.grid(row= 1, column= 1)
-
-
 
 label = tkinter.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 45, "normal"), bg=YELLOW)
 label.grid(row= 0, column= 1)
